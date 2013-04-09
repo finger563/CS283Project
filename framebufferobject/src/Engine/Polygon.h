b47cccaaf07b39dd017fcc25fac536e1fab872cd
William Emfinger <waemfinger@gmail.com>
2013-04-09 13:16:28 -0500

    Updating code for rewrite.

---
+++ framebufferobject/src/Engine/Polygon.h
@@ -0,0 +1,75 @@
+#ifndef	Polygon_H
+#define	Polygon_H
+
+#include "Vertex.h"
+
+#define POLY_MAX_VERTICES 4		// don't want anything other than tris and quads
+
+enum PolyType {
+    FLAT_TOP_RIGHT,
+    FLAT_TOP_LEFT,
+    FLAT_BOTTOM_RIGHT,
+    FLAT_BOTTOM_LEFT,
+    NORMAL_RIGHT,
+    NORMAL_LEFT
+};
+
+class Poly
+{
+public:
+	Vertex vertices[POLY_MAX_VERTICES];
+	int numVertices;
+	Vector3D normal;
+    TriangleType type;  // for fast rendering, pre-compute
+	bool visible;		// for backface culling
+
+    // Screen space variables for Z-buffering
+    Point3D s1,s2,s3,s4;		// screen space coordinates for triangle (y-sorted)
+    float m12,m13,m23;			// screen space slopes (dx/dy)
+    float sx,ex;				// start and end x-pixel coordinates
+	float syl,eyl,syr,eyr;		// start and end y-pixel coordinates (not currently used)
+	float sz,ez;				// start and end 1/z world coordinates of object (per scanline)
+    float dzx,dz13,dz12,dz23,dz14,dz43;    // z-slopes for 1/z-buffering
+	
+	// Variables for Texturing
+	Point2D u,v,w;							// texture coords for each point 
+	const unsigned short* texture;			// for texturing
+	int texwidth;							// width of texture
+	Point2D u1,u2,u3,u4;					// screen space coordinates for texture
+	float stu,stv,etu,etv;					// start and end x & y texture coordinates per scanline
+    float dtx13,dtx12,dtx23,dtx14,dtx43;    // slopes for x texture coordinates
+    float dty13,dty12,dty23,dty14,dty43;    // slopes for y texture coordinates
+
+    Poly(){a=b=c=Point3D();normal=Vector3D();}
+    Poly(const Point3D _a,
+			 const Point3D _b,
+			 const Point3D _c, 
+			 const Vector3D n, 
+			 short clr) {
+        a=_a;b=_b;c=_c;normal=n;color=clr;
+    }
+    Poly(const Point3D _a,
+			 const Point3D _b,
+			 const Point3D _c, 
+			 const Vector3D n, 
+			 const Point2D _u,
+			 const Point2D _v,
+			 const Point2D _w) {
+        a=_a;b=_b;c=_c;normal=n;u=_u;v=_v;w=_w;
+    }
+    ~Poly(){}
+
+    void Print(){}
+	void SetTexture(const unsigned short *tex, const int width) { texture = tex; texwidth = width; }
+
+    void Rotate( const Matrix& m );
+    void Translate( const Vector3D& v );
+    void Translate( const float x, const float y, const float z );
+
+    void TransformToScreen( const Matrix& m );
+
+    void DrawFilledZbuffer( const int y );
+    void DrawTexturedZbuffer( const int y );
+};
+
+#endif		// Polygon_H
--- framebufferobject/src/Engine/Vertex.h
+++ framebufferobject/src/Engine/Vertex.h
@@ -5,19 +5,39 @@
 #include <stdlib.h>
 #include <math.h>
 #include "..\main.h"
+#include "matrix.h"
 
 class Vertex
 {
 public:
-    float x,y,z,w;	// 3D homogeneous coords
-	int u,v;		// 2D texture coords
-	float r,g,b;	// RGB color (range = [0,1])
-	float nx,ny,nz;	// components of normal vector
+    float x,y,z,w;		// 3D homogeneous coords
+	float ex,ey,ez,ew;	// 3D homogenous eye/camera coords
+	int u,v;			// 2D texture coords
+	float r,g,b;		// RGB color (range = [0,1])
+	float nx,ny,nz,nw;	// components of normal vector
 
 
-    Vertex() {x=y=z=0;w=1;}
-    Vertex(const float _x,const float _y,const float _z) { x = _x; y = _y; z = _z; w = 1; }
-    Vertex(const float _x,const float _y,const float _z,const float _w) { x = _x; y = _y; z = _z; w = _w; }
+    Vertex() {
+		x=y=z=0;w=1;
+		ex=ey=ez=0;ew=1;
+		u=v=0;
+		r=g=b=0;
+		nx=ny=nz=0;nw=1;
+	}
+    Vertex(const float _x,const float _y,const float _z) { 
+		x = _x; y = _y; z = _z; w = 1; 
+		ex=ey=ez=0;ew=1;
+		u=v=0;
+		r=g=b=0;
+		nx=ny=nz=0;nw=1;
+	}
+    Vertex(const float _x,const float _y,const float _z,const float _w) { 
+		x = _x; y = _y; z = _z; w = _w; 
+		ex=ey=ez=0;ew=1;
+		u=v=0;
+		r=g=b=0;
+		nx=ny=nz=0;nw=1;
+	}
     ~Vertex(){}
 
     void Print() {}
